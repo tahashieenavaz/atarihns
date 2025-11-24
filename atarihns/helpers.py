b3718d4cd40d9e531ec32710f9a5fcacdc32ea73
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-24 15:10:20 +0100

    refines environment name handling

--- atarihns/helpers.py
+++ atarihns/helpers.py
@@ -1,15 +1,33 @@
 from .constants import ATARI_SCORES
 
 
-def get_human_score(environment: str) -> float:
-    return ATARI_SCORES.get(environment)[1]
+def refine_environment_name(environment_name: str):
+    if environment_name.startswith("ALE/"):
+        return environment_name[4:]
+
+    return environment_name
+
+
+def check_environment_statistics(environment_name: str):
+    if environment_name not in ATARI_SCORES:
+        raise RuntimeError(
+            f"We couldn't find the statistics regarding atari environment {environment_name}."
+        )
+
+
+def get_human_score(environment_name: str) -> float:
+    environment_name = refine_environment_name(environment_name)
+    check_environment_statistics(environment_name)
+    return ATARI_SCORES.get(environment_name)[1]
 
 
 def get_random_score(environment: str) -> float:
+    environment_name = refine_environment_name(environment_name)
+    check_environment_statistics(environment_name)
     return ATARI_SCORES.get(environment)[0]
 
 
-def calculate_hns(environment: str, agent_score: float) -> float:
-    human_score = get_human_score(environment)
-    random_score = get_random_score(environment)
+def calculate_hns(environment_name: str, agent_score: float) -> float:
+    human_score = get_human_score(environment_name)
+    random_score = get_random_score(environment_name)
     return (agent_score - random_score) / (human_score - random_score)
