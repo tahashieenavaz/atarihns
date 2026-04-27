4ee3ab24f6aa990c85b3259abbf3ec770c9d3aaf
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-27 13:16:37 +0200

    version

--- atarihns/__init__.py
+++ atarihns/__init__.py
@@ -3,3 +3,5 @@ from .functions import get_random_score
 from .functions import calculate_hns
 from .functions import calculate_hns as hns
 from .functions import calculate_hns as get_hns
+
+__version__ = "1.0.0"
