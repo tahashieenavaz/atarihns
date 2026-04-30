d31ac8ba97cd9fdddf0985859308cbec4778b27d
tahashieenavaz <tahashieenavaz@gmail.com>
2026-04-30 15:05:54 +0200

    adds environment class

---
+++ atarihns/Environment.py
@@ -0,0 +1,15 @@
+from .constants import ATARI_SCORES
+
+
+class Environment:
+    def __init__(self, environment: str):
+        pass
+
+    def human_score(self):
+        pass
+
+    def random_score(self):
+        pass
+
+    def hns(self):
+        pass
--- atarihns/__init__.py
+++ atarihns/__init__.py
@@ -3,5 +3,6 @@ from .functions import get_random_score
 from .functions import calculate_hns
 from .functions import calculate_hns as hns
 from .functions import calculate_hns as get_hns
+from .Environment import Environment
 
 __version__ = "1.0.0"
